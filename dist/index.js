c68e7b61d9bdca19b44872219b31db3320247c92
muendokennedy <kennedymunyao999@gmai.com>
2024-09-26 14:02:11 +0300

    Learnt about optional chaining

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,8 @@
 "use strict";
-function greet(name) {
-    if (name) {
-        console.log(name.toUpperCase());
-    }
-    else {
-        console.log('Holla');
-    }
+var _a;
+function getCustomer(id) {
+    return id === 0 ? null : { birthDay: new Date() };
 }
-greet('Kennedy Munyao');
+let customer = getCustomer(24);
+console.log((_a = customer === null || customer === void 0 ? void 0 : customer.birthDay) === null || _a === void 0 ? void 0 : _a.getFullYear());
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
-// Nullable types
+// Learning about optional chaining
 
-function greet(name: string | null | undefined){
-
-  if(name){
-    console.log(name.toUpperCase())
-  } else {
-    console.log('Holla')
-  }
+type Customer = {
+  birthDay?: Date
 }
 
-greet('Kennedy Munyao')
+function getCustomer(id: number): Customer | null | undefined{
+  return id === 0 ? null : { birthDay: new Date()}
+}
+
+let customer = getCustomer(24)
+
+console.log(customer?.birthDay?.getFullYear())
